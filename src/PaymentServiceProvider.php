824c5ee8733cfe704ca7b6e2a7340ab5f4b7f0ad
Atiqur Rahman <atiq.misterrahman@gmail.com>
2024-10-07 12:30:19 +0600

    feat: added constructor properties while creating instance

--- src/PaymentServiceProvider.php
+++ src/PaymentServiceProvider.php
@@ -19,7 +19,14 @@ class PaymentServiceProvider extends ServiceProvider
         $this->mergeConfigFrom(__DIR__.'/../config/spayment.php', 'spayment');
 
         $this->app->singleton(Nagad::class, function ($app) {
-            return new Nagad;
+            return new Nagad(
+                config('spayment.gateways.nagad.merchant_id'),
+                config('spayment.gateways.nagad.merchant_public_key'),
+                config('spayment.gateways.nagad.merchant_private_key'),
+                config('spayment.gateways.nagad.merchant_hex'),
+                config('spayment.gateways.nagad.merchant_iv'),
+                config('spayment.gateways.nagad.merchant_number')
+            );
         });
     }
 }
